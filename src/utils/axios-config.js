f4f9be6c361f81f858ee26834e2dd7daf85fd4da
UpHeroK <jhon2170098@correo.uis.edu.co>
2023-08-18 16:51:58 -0500

    error rutas

--- src/utils/axios-config.js
+++ src/utils/axios-config.js
@@ -1,30 +1,31 @@
 import axios from 'axios';
 import router from '@/router';
 
-axios.defaults.baseURL = 'http://localhost:8000/api/';
+// axios.defaults.baseURL = 'http://localhost:8000/api/'; local
+axios.defaults.baseURL = 'https://test-api.herotest.net/api/';
 
 axios.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem('token');
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => Promise.reject(error)
+    (config) => {
+        const token = localStorage.getItem('token');
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => Promise.reject(error)
 );
 
 axios.interceptors.response.use(
-  response => {
-    return response;
-  },
-  error => {
-    if (error.response && error.response.status === 401 && router.currentRoute.path !== '/login') {
-      localStorage.removeItem('token');
-      router.push('/login');
+    response => {
+        return response;
+    },
+    error => {
+        if (error.response && error.response.status === 401 && router.currentRoute.path !== '/login') {
+            localStorage.removeItem('token');
+            router.push('/login');
+        }
+        return Promise.reject(error);
     }
-    return Promise.reject(error);
-  }
 );
 
-export default axios;  
+export default axios;
